b5d0e4c3f1a101023b9379b1090311b796523869
Andrey Oblivantsev <eslider@gmail.com>
2017-02-28 13:52:17 +0100

    Remove empty list elements method

--- MapbenderDataSourceBundle.php
+++ MapbenderDataSourceBundle.php
@@ -5,17 +5,9 @@ use Mapbender\CoreBundle\Component\MapbenderBundle;
 
 /**
  * DataSource Bundle.
- * y
+ * 
  * @author Andriy Oblivantsev
  */
 class MapbenderDataSourceBundle extends MapbenderBundle
 {
-    /**
-     * @inheritdoc
-     */
-    public function getElements()
-    {
-        return array(
-        );
-    }
 }
